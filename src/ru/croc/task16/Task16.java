0b9314dfd35d34eb64ec47462222a6fd4aa3276e
DenisStepanidenko <110686828+DenisStepanidenko@users.noreply.github.com>
2022-11-30 01:53:58 +0300

    Update Task16.java

--- src/ru/croc/task16/Task16.java
+++ src/ru/croc/task16/Task16.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 public class Task16 {
     public static void main(String[] args) throws IOException {
-        //String path = args[0]; // путь к директории
-        String path = "C:/Users/stepa/IdeaProjects/dsStepanidenko/src/ru/croc/Task16/Новая папка";
+        String path = args[0]; // путь к директории
+        //String path = "C:/Users/stepa/IdeaProjects/dsStepanidenko/src/ru/croc/Task16/Новая папка";
         ru.croc.task16.mergingLogs.MergeLogs.getResult(path);
     }
 }
