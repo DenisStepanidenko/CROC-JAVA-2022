a5e764f8a56052e4b47f128c123953a5b18b6a5f
DenisStepanidenko <110686828+DenisStepanidenko@users.noreply.github.com>
2022-12-03 15:50:16 +0300

    Add files via upload

---
+++ src/ru/croc/task13/DataSet.java
@@ -0,0 +1,23 @@
+package ru.croc.task13.some_package;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
+public class DataSet{
+    private Map<Integer , String> films;
+    private Map<Integer, ArrayList<Integer>> views;
+
+    public DataSet(String pathToTheFilm, String pathToTheViews) throws IOException {
+        films = Parser.parseFilms(pathToTheFilm);
+        views = Parser.parseViews(pathToTheViews);
+    }
+
+    public Map<Integer, String> getFilms() {
+        return films;
+    }
+
+    public Map<Integer, ArrayList<Integer>> getViews() {
+        return views;
+    }
+}
--- src/ru/croc/task13/Task13.java
+++ src/ru/croc/task13/Task13.java
@@ -5,11 +5,13 @@ import java.io.IOException;
 import java.util.Scanner;
 public class Task13 {
     public static void main(String[] args) throws IOException {
-        Scanner F = new Scanner(System.in);
+        Scanner f = new Scanner(System.in);
         System.out.println("Введите пожалуйста список просмотров по которому хотите получить рекомендацию. Вводите с разделением запятой и без пробелов");
-        String films = F.next();
-        String answer = FindingRecommendations.FindRecommendations(films);
-        System.out.println(answer);
-        // на тестах который дан в условии задачи был выдан верный ответ, ура!!
+        String filmsOfUser = f.next();
+        String pathToTheFilms = "C:/Users/stepa/IdeaProjects/Croc/src/ru/croc/task13/films.txt";
+        String pathToTheViews = "C:/Users/stepa/IdeaProjects/Croc/src/ru/croc/task13/history.txt";
+        FindingRecommendations test = new FindingRecommendations(pathToTheFilms , pathToTheViews);
+        System.out.println(test.getPopularFilmForUser(filmsOfUser));
+
     }
 }
