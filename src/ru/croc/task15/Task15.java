02eb406492d96bcf1bdcc79049521e028ca228e3
DenisStepanidenko <110686828+DenisStepanidenko@users.noreply.github.com>
2022-12-07 12:41:04 +0300

    Add files via upload

--- src/ru/croc/task15/Task15.java
+++ src/ru/croc/task15/Task15.java
@@ -1,11 +1,14 @@
 package ru.croc.task15;
 
+import ru.croc.task15.timeCounting.ParseAndSearchingRoot;
+
 import java.io.IOException;
-import ru.croc.task15.TimeCounting.TimeCounting;
+
 public class Task15 {
     public static void main(String[] args) throws IOException {
-        String filePath = "C:/Users/stepa/IdeaProjects/Task15/src/Текстовый документ.txt"; //создаём путь к файлу, я тестил файл, который в условии задачи и ещё немного его изменял
-        TimeCounting.getResult(filePath); //собственно самая функция, которая выводит результат
+        String filePath = "C:/Users/stepa/IdeaProjects/Croc/src/ru/croc/task15/Текстовый документ.txt"; //создаём путь к файлу, я тестил файл, который в условии задачи и ещё немного его изменял
+        int answer = ParseAndSearchingRoot.parseAndSearchingRoot(filePath).totalProcessTime();
+        System.out.println(answer);
         // данная функция работает так, что не важно в каком порядке вообще идут отделы в файле
     }
 }
