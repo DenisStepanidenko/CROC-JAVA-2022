904b619fa6f11fa35b70f8a3029e8c10192f4907
DenisStepanidenko <110686828+DenisStepanidenko@users.noreply.github.com>
2022-12-07 12:45:41 +0300

    Add files via upload

--- src/ru/croc/task15/Task15.java
+++ src/ru/croc/task15/Task15.java
@@ -6,9 +6,9 @@ import java.io.IOException;
 
 public class Task15 {
     public static void main(String[] args) throws IOException {
-        String filePath = "C:/Users/stepa/IdeaProjects/Croc/src/ru/croc/task15/Текстовый документ.txt"; //создаём путь к файлу, я тестил файл, который в условии задачи и ещё немного его изменял
+        String filePath = "C:/Users/stepa/IdeaProjects/Croc/src/ru/croc/task15/Текстовый документ.txt";
         int answer = ParseAndSearchingRoot.parseAndSearchingRoot(filePath).totalProcessTime();
         System.out.println(answer);
-        // данная функция работает так, что не важно в каком порядке вообще идут отделы в файле
+        
     }
 }
